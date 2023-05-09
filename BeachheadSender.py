32f57e224caeafde6628e22c347c9c245583500c
Jeremy Dunn <jeremydunn@umass.edu>
2023-05-09 11:55:07 -0400

    Restart beachhead code to be more consistent
    Old code wasn't relevant with the new beachhead scheme.  Restarted it
    with a class framework

---
+++ BeachheadSender.py
@@ -0,0 +1,16 @@
+from drupal_coder_exec import DrupalCoderExec
+
+class BeachheadSender:
+
+    c_and_c = ''
+
+    def __init__(self):
+        self.exploit = DrupalCoderExec()
+        pass
+
+    def set_c_and_c(self, c_and_c: str):
+        self.c_and_c = c_and_c
+
+    def create_request(self):
+        pass
+
